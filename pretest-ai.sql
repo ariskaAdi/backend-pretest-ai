9db462e9c43081d0c1c8feba894359bd41a3385c
ariskaAdi <ariskaadiprasetyo@gmail.com>
2026-04-06 17:08:01 +0700

    add review

---
+++ pretest-ai.sql
@@ -0,0 +1,4 @@
+UPDATE modules
+SET file_url = REPLACE(file_url, 'https://pub-8bd534adf5314f1aa5f5c309346548c0.r2.dev/modules/3691d66f_CV ARISKA ADI PRASETYO.docx (2).pdf', 'https://docs.pretestai.my.id/modules/3691d66f_CV%20ARISKA%20ADI%20PRASETYO.docx%20(2).pdf');
+
+select * from modules;
